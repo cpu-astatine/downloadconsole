bffc8277c831dfe7404d42aa065910b7afe5c7e8
Astatine <87228325+cpu-astatine@users.noreply.github.com>
2023-07-13 03:57:57 -0400

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -3,4 +3,6 @@ sudo apt-get upgrade -y
 sudo apt-get install python3 -y
 sudo apt-get install python3-pip -y
 sudo apt-get install python3-tk -y
+sudo apt-get install wget -y
+sudo apt-get install git -y
 echo "All programs are installed successfully"
